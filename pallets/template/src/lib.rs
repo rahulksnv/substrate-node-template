a59c1397a3060bc5060eea8a60bab1a28c8907d9
Rahul Subramaniyam <rahulksnv@gmail.com>
2023-02-07 14:28:52 -0600

    Add pellet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,18 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/reference/frame-pallets/>
+// Re-export pallet items so that they can be accessed from the crate namespace.
 pub use pallet::*;
-
-#[cfg(test)]
-mod mock;
-
-#[cfg(test)]
-mod tests;
-
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -23,82 +12,72 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/main-docs/build/runtime-storage/
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
-
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored { something: u32, who: T::AccountId },
+		/// User added.
+		UserAdded { who: T::AccountId },
+		/// User removed.
+		UserRemoved { who: T::AccountId },
+		/// User info changed.
+		UserInfoChanged { who: T::AccountId, x: i128, y: i128},
 	}
 
-	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		/// User already exists.
+		UserExists,
+		/// User not found.
+		UserNotFound,
 	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::storage]
+	pub(super) type UserMap<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, (T::BlockNumber, Option<(i128, i128)>)>;
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::call_index(0)]
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/main-docs/build/origins/
-			let who = ensure_signed(origin)?;
+		#[pallet::weight(0)]
+		pub fn add_user(origin: OriginFor<T>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			ensure!(!UserMap::<T>::contains_key(&sender), Error::<T>::UserExists);
 
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored { something, who });
-			// Return a successful DispatchResultWithPostInfo
+			let block_number = <frame_system::Pallet<T>>::block_number();
+			let val: (T::BlockNumber, Option<(i128, i128)>) = (block_number, None);
+			UserMap::<T>::insert(&sender, val);
+			Self::deposit_event(Event::UserAdded { who: sender });
 			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
 		#[pallet::call_index(1)]
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		#[pallet::weight(0)]
+		pub fn remove_user(origin: OriginFor<T>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			ensure!(UserMap::<T>::contains_key(&sender), Error::<T>::UserNotFound);
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
+			UserMap::<T>::remove(&sender);
+			Self::deposit_event(Event::UserRemoved { who: sender });
+			Ok(())
+		}
+
+		#[pallet::call_index(2)]
+		#[pallet::weight(0)]
+		pub fn update_user_info(origin: OriginFor<T>, x: i128, y: i128) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			ensure!(UserMap::<T>::contains_key(&sender), Error::<T>::UserNotFound);
+
+			let block_number = <frame_system::Pallet<T>>::block_number();
+			let val: (T::BlockNumber, Option<(i128, i128)>) = (block_number, Some((x, y)));
+			UserMap::<T>::set(&sender, Some(val));
+			Self::deposit_event(Event::UserInfoChanged { who: sender, x, y });
+			Ok(())
 		}
 	}
 }
+
+
